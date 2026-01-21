f6abab46f8bcb3e69c47056fc728a6ac083f0f4f
src_resources <src.res.211@gmail.com>
2026-01-21 16:23:10 +0800

    Update

---
+++ assets/connectors_false-C3kAbdMi.js
@@ -0,0 +1,1 @@
+const o={};throw new Error('Could not resolve "@safe-global/safe-apps-sdk" imported by "@wagmi/connectors".');export{o as default};
---
+++ assets/connectors_false-COjPGUDX.js
@@ -0,0 +1,1 @@
+const o={};throw new Error('Could not resolve "@base-org/account" imported by "@wagmi/connectors".');export{o as default};
